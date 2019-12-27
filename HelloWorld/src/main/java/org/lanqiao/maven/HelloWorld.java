16838615e9a7a2845b3bfd511ef914b8ba103c7a
ChinaHJJ <59202072+hjj12345678@users.noreply.github.com>
2019-12-27 13:26:17 +0800

    Update HelloWorld.java
    2

--- HelloWorld/src/main/java/org/lanqiao/maven/HelloWorld.java
+++ HelloWorld/src/main/java/org/lanqiao/maven/HelloWorld.java
@@ -2,6 +2,7 @@ package  org.lanqiao.maven;
 public  class HelloWorld{
 	public  String  sayHello(String name){
 		 System.out.println("嗨git");
+			 System.out.println("git奶奶的");
 		return  "Hello "+name;
 	}
 }
